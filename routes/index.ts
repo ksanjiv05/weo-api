a71d64efec1bfe536115c65e51dfbd5430dcad86
sanjiv kumar pandit <kumarsanjiv0005@gmail.com>
2022-08-01 10:05:47 -0700

    initila commit

---
+++ routes/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+// import { login } from "../controllers/authController/auth";
+const router = express.Router();
+
+// router.post("/login",login)
+
+export default router;
