c9b86551244b4d07de03749e76c2e22d0b7ec1d8
SANJIV <sanjiv@SANJIVs-MacBook-Air.local>
2024-01-16 09:34:11 +0530

    type error fix

--- controllers/oCalculatorController/oCalculator.ts
+++ controllers/oCalculatorController/oCalculator.ts
@@ -1,16 +1,16 @@
 import { Response, Request } from "express";
 import { responseObj } from "../../helper/response";
 
-const getOPrice = (req: Request, res: Response) => {
-  try {
-    const oPrice = 10; //get o price
-    responseObj({
-      resObj: res,
-      type: "success",
-      statusCode: 200,
-      msg: "here your latest o price ",
-    });
-  } catch (error) {
-    responseObj({});
-  }
-};
+// const getOPrice = (req: Request, res: Response) => {
+//   try {
+//     const oPrice = 10; //get o price
+//     responseObj({
+//       resObj: res,
+//       type: "success",
+//       statusCode: 200,
+//       msg: "here your latest o price ",
+//     });
+//   } catch (error) {
+//     responseObj({});
+//   }
+// };
